b22534523e5b0719e078ccbfdd320a93353ea91e
eng-salem <eng.salem.ahmed@gmail.com>
2026-04-10 22:15:41 +0200

    jhg

--- PrayerWidget/Services/TursoConstants.vb
+++ PrayerWidget/Services/TursoConstants.vb
@@ -7,6 +7,5 @@ Public NotInheritable Class TursoConstants
 
     ' Counter database - used for device_counters and quotes
     Public Const CounterTursoUrl As String = "https://counter-manhag.aws-eu-west-1.turso.io"
-    ' IMPORTANT: Replace with actual token or load from environment/config
     Public Const CounterAuthToken As String = "YOUR_TURSO_AUTH_TOKEN_HERE"
 End Class
